96c4c1fbeef95c012dc51270ee578fb83beb1c4c
AJ Strock <strockaj320@gmail.com>
2023-03-15 16:43:02 -0400

    Frontend now calls backend

--- Frontend/TarkovFrontEnd/Form1.Designer.cs
+++ Frontend/TarkovFrontEnd/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(255, 241);
+            button1.Name = "button1";
+            button1.Size = new Size(279, 174);
+            button1.TabIndex = 0;
+            button1.Text = "Query";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(348, 76);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
     }
 }
--- Frontend/TarkovFrontEnd/Form1.cs
+++ Frontend/TarkovFrontEnd/Form1.cs
@@ -1,3 +1,10 @@
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Net;
+using static System.Net.WebRequestMethods;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
 namespace TarkovFrontEnd
 {
     public partial class Form1 : Form
@@ -5,6 +12,34 @@ namespace TarkovFrontEnd
         public Form1()
         {
             InitializeComponent();
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            string iconLink;
+            string item = "M.U.L.E";
+            string url = "http://127.0.0.1:5000/?item=" + item;
+
+            using var client = new HttpClient();
+            var content = await client.GetAsync(url);
+            content.EnsureSuccessStatusCode();
+            var responseBody = await content.Content.ReadAsStringAsync();
+            var myJObject = JObject.Parse(responseBody);
+            try
+            {
+                iconLink = myJObject.SelectToken("data").SelectToken("items").SelectToken("iconLink").ToString();
+                var icon = new Bitmap(iconLink);
+                pictureBox1.Image = icon;
+            } catch (Exception ex)
+            {
+                iconLink = "";
+                Debug.WriteLine(ex.Message);
+            }
+            
+            
+
+            Debug.WriteLine(myJObject.SelectToken("data").SelectToken("items"));
+
         }
     }
 }
